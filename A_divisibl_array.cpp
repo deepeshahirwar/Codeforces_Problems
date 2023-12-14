58eb596456c27b1ebfe17fbeaf8fb2ec87258c2a
deepesh798G <deepeshahirwar7024@gmail.com>
2023-12-14 16:16:43 +0530

    new two file added

---
+++ A_divisibl_array.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+#include <cctype>
+#include <iostream>
+#include <cstring> 
+
+#define ll long long
+#define pb push_back
+#define fastread() (ios_base::sync_with_stdio(false), cin.tie(NULL));
+using namespace std;
+
+int main()
+{
+    fastread(); 
+
+    int t; cin>>t; 
+    while(t--){ 
+
+    int n; cin>>n;  
+    for(int i=1; i<=n; i++){ 
+      cout<< 2*i<<" "; 
+    }   
+    cout<<endl;
+
+    }
+    return 0;
+}
+ 
+ 
+           
