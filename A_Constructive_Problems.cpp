7e624d9e25f845d424d01764d045354ccb5aef9e
Deepesh7987 <116001106+deepeshahirwar@users.noreply.github.com>
2023-12-17 12:52:27 +0530

    round 915 div.2

---
+++ A_Constructive_Problems.cpp
@@ -0,0 +1,19 @@
+
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define fastread() (ios_base::sync_with_stdio(false), cin.tie(NULL));
+using namespace std;
+
+int main()
+{
+    fastread();
+    int t;
+    cin >> t; 
+    while(t--){
+        int n,m; cin>>n>>m; 
+        cout<<max(n,m)<<endl; 
+
+    }
+    return 0;   
+}
