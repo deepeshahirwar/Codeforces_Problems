30e0e16b1f471372728c82ba3268ffeb95e3ff65
deepeshahirwar <deepeshahirwar7024@gmail.com>
2024-11-27 13:10:28 +0530

    new problem added

---
+++ newbie/C_clock_and_string.cpp
@@ -0,0 +1,66 @@
+/******************** DEEPESH AHIRWAR ***********************/
+#include<bits/stdc++.h> 
+ #include <fstream> 
+ using namespace std; 
+
+ #define fastio() ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+ #define MOD 1000000007
+
+ 
+ #define endl "\n"
+ #define pb push_back
+ #define ppb pop_back
+ #define mp make_pair
+ #define ff first
+ #define ss second
+ #define set_bits __builtin_popcountll
+ #define sz(x) ((int)(x).size())
+ #define all(x) (x).begin(), (x).end()  
+ #define sort_all(x) sort(x.begin(),x.end())
+ #define reverse_all(x) reverse(x.begin(),x.end())
+ #define py cout<<"YES"<<"\n" 
+ #define pn cout<<"NO"<<"\n" 
+ 
+ typedef long long ll;
+ typedef unsigned long long ull;
+ typedef long double lld; 
+
+/*************** WELCOME *******************/
+  void solution(){
+      int a,b,c,d; 
+      cin>>a>>b>>c>>d; 
+     bool ok1=false,ok2 = false; 
+
+     for(int i = min(a,b); i<=max(a,b); i++){
+          if( i==c){
+            ok1 = true;
+          } 
+          if(i == d){
+            ok2 = true;
+          }
+     } 
+
+     if(ok1 != ok2){
+        py;
+     }else{
+        pn;
+     }
+     
+
+  } 
+
+ int main() {
+ #ifndef ONLINE_JUDGE 
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+ #endif  
+fastio();    
+
+   ll t; 
+    cin>>t;  
+   for(int i=0;i<t;i++){ 
+    solution(); 
+   } 
+
+   return 0;
+ }
